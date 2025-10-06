1dc6cca539517be744d3d4a986c4a19e0afc163a
admin <n.miteiko@redmadrobot.com>
2025-10-06 19:55:32 +0300

    add new struct // remove files

--- sgr_deep_research/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Services module for external integrations and business logic."""
-
-from sgr_deep_research.services.tavily_search import TavilySearchService
-
-__all__ = [
-    "TavilySearchService",
-]
